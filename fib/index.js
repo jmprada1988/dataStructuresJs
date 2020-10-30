ab06b9e641fadca9910c9166e190ea4a3e044e7c
davirlein1988 <lein.davir@gmail.com>
2020-10-29 23:01:36 -0500

    added simple fibonacci recursive solution

--- fib/index.js
+++ fib/index.js
@@ -7,7 +7,10 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
-
-function fib(n) {}
+// simple recursive solution
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 2) + fib(n - 1);
+}
 
 module.exports = fib;
