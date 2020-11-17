77d34f7d71e8bf5386ce8f890a4f126dbe30144c
davirlein1988 <lein.davir@gmail.com>
2020-11-16 22:38:55 -0500

    Code formatting old docs

--- addUpTo.js
+++ addUpTo.js
@@ -1,13 +1,13 @@
-function addUpTo(n) { 
-	let total = 0;
-	for (let i = 1; i<= n; i++) {
-	total += i;
-	}
-	return total;
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
 }
 
-function SecAddUpTo(n){
-	return n * (n + 1) / 2;
+function SecAddUpTo(n) {
+  return (n * (n + 1)) / 2;
 }
 
 let t1 = performance.now();
@@ -18,5 +18,5 @@ let t3 = performance.now();
 console.log(SecAddUpTo(100000000000));
 let t4 = performance.now();
 
-console.log(`Time elapsed: ${(t2 - t1)/ 1000} seconds for first method`);
-console.log(`Time elapsed: ${(t4 - t3)/ 1000} seconds for second method`);
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds for first method`);
+console.log(`Time elapsed: ${(t4 - t3) / 1000} seconds for second method`);
