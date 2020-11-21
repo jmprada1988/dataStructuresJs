93754fbb80f752e2311e72f0177851876ee2ae35
davirlein1988 <lein.davir@gmail.com>
2020-11-21 03:20:58 -0500

    added remve methods

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -38,6 +38,27 @@ class LinkedList {
     }
     return last;
   }
+  clear() {
+    this.head = null;
+  }
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+  removeLast() {
+    if (!this.head) return;
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let prev = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      prev = node;
+      node = node.next;
+    }
+    prev.next = null;
+  }
 }
 const list = new LinkedList();
 
