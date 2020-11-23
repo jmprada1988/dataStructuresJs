344b0b5484e50cfc7a606b828c99c7e8c1af2b61
davirlein1988 <lein.davir@gmail.com>
2020-11-22 23:33:20 -0500

    Added unit testing to command pattern

--- designPatterns/command/test.js
+++ designPatterns/command/test.js
@@ -21,3 +21,21 @@ test("Handler exist", () => {
   expect(handler).toBeDefined();
   expect(typeof handler).toEqual("object");
 });
+test("Aritmetic operation work correctly", () => {
+  expect(handler.perform("add", 2, 6)).toEqual(8);
+  expect(handler.perform("subtract", 2, 6)).toEqual(-4);
+  expect(handler.perform("multiply", 2, 6)).toEqual(12);
+  expect(handler.perform("divide", 24, 6)).toEqual(4);
+});
+
+test("it handles mathematical edge cases", () => {
+  expect(() => {
+    handler.perform("add", 0, 0);
+  }).not.toThrow();
+  expect(() => {
+    handler.perform("divide", 20, 0);
+  }).not.toThrow();
+  expect(() => {
+    handler.perform("divide", 0, 20);
+  }).not.toThrow();
+});
