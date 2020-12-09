3114ef2e1ce320d77b1363e766d9e512fa22ac88
davirlein1988 <lein.davir@gmail.com>
2020-12-09 01:13:48 -0500

    Binary search tree implemetation complete

--- bst/index.js
+++ bst/index.js
@@ -29,6 +29,15 @@ class Node {
     }
     
   }
+  contains(data) {
+    if(this.data === data) return this;
+    if(this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if(this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+    return null;
+  }
 }
 
 module.exports = Node;
