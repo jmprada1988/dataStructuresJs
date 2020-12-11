849e6b2d308976e8597f510986b83b3e5182ed70
davirlein1988 <lein.davir@gmail.com>
2020-12-11 01:54:18 -0500

    Added bubble sort solution

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,27 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+  //variable to optimaze iterations unneeded
+  let noSwaps = true;
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
