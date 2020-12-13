56428f4925b38f951a44184ec21e43de162a945d
davirlein1988 <lein.davir@gmail.com>
2020-12-12 22:05:52 -0500

    Added helper swapper

--- sorting/index.js
+++ sorting/index.js
@@ -1,10 +1,12 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+// Helper method to swap two indexes in array.
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
 function bubbleSort(arr) {
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-  };
   //variable to optimaze iterations unneeded
   let noSwaps = true;
   for (let i = arr.length; i > 0; i--) {
@@ -19,7 +21,16 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        swap(arr, arr[j], arr[min]);
+      }
+    }
+  }
+}
 
 function mergeSort(arr) {}
 
