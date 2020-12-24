60efcd3e72f0e4b8dc83553d75582403476b3b8e
davirlein1988 <lein.davir@gmail.com>
2020-12-24 14:46:31 -0300

    Passed tests for selection sort

--- sorting/index.js
+++ sorting/index.js
@@ -3,8 +3,9 @@
 
 // Helper method to swap two indexes in array.
 const swap = (arr, idx1, idx2) => {
-  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-};
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  },
+  data = [89, 1, 2, 3, 48, 5, 6, 7, 10, 45, 56, 67, 87];
 
 function bubbleSort(arr) {
   //variable to optimaze iterations unneeded
@@ -22,14 +23,18 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  let noSwap = true;
   for (let i = 0; i < arr.length; i++) {
     let min = i;
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[min]) {
-        swap(arr, arr[j], arr[min]);
+        swap(arr, min, j);
+        noSwap = false;
       }
     }
+    if (noSwap) continue;
   }
+  return arr;
 }
 
 function mergeSort(arr) {}
