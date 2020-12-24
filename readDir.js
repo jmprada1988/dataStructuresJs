c2443c1db7dbcca674f9615ea92cb3fa919192d5
davirlein1988 <lein.davir@gmail.com>
2020-12-24 19:49:55 -0300

    Ls implementation in node with path and fs

---
+++ readDir.js
@@ -0,0 +1,17 @@
+const fs = require("fs").promises,
+  path = require("path");
+
+const listDir = async (dirpath) => {
+  let dir = await fs.opendir(dirpath);
+  for await (let entry of dir) {
+    let name = entry.name;
+    if (entry.isDirectory()) {
+      name += "/";
+    }
+    let stats = await fs.stat(path.join(dirpath, name)),
+      size = stats.size;
+    console.log(String(size).padStart(10), name);
+  }
+};
+
+listDir("/home/nenuser/JAVASCRIPT/dataStructuresJs");
